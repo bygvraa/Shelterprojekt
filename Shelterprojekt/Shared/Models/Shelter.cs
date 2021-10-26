8344e90df3c09fd5f7169ec9ad4a0748c14a7fc9
mikkel <eaamiby@students.eaaa.dk>
2021-10-26 22:14:34 +0200

    Splittelse af 'properties' fra Sbelter, så 'properties' kan indlæses ordentligt

---
+++ Shelterprojekt/Shared/Models/Shelter.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Shelterprojekt.Shared
+{
+    [BsonIgnoreExtraElements]
+    public class Shelter
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("properties")]
+        public Properties properties { get; set; }
+
+    }
+
+    [BsonIgnoreExtraElements]
+    public class Properties
+    {
+        [BsonElement("navn")]
+        public string navn { get; set; }
+        public string cvr_navn { get; set; }
+        public double? antal_pl { get; set; }
+        public double? postnr { get; set; }
+        public string vejnavn { get; set; }
+        public string kontakt_ved { get; set; }
+        public string handicap { get; set; }
+    }
+}
