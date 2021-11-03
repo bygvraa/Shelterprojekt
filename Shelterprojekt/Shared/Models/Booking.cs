ace75f77f6498d1e213df8c2c6898ae8888f7cfd
mikkel <eaamiby@students.eaaa.dk>
2021-11-03 20:04:05 +0100

    Tilføjelse af data validerings-kvaliteter til data typer

--- Shelterprojekt/Shared/Models/Booking.cs
+++ Shelterprojekt/Shared/Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -10,9 +11,17 @@ namespace Shelterprojekt.Shared.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
         public string shelterId { get; set; }
+        
         public string shelterNavn { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        [Required(ErrorMessage = "Indtast venligst en gyldig dato")]
         public DateTime dato { get; set; }
+        
+        [Required(ErrorMessage = "Indtast venligst et brugernavn")]
+        [MinLength(2, ErrorMessage = "Brugernavnet er for kort")]
         public string brugerNavn { get; set; }
     }
 
