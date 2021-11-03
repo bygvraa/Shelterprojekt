c15b895a63cb465fa80c8bd6175ed07816b285f9
mikkel <eaamiby@students.eaaa.dk>
2021-11-03 20:03:25 +0100

    Omdøbelse af klasse

---
+++ Shelterprojekt/Shared/Models/ShelterDatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace Shelterprojekt.Shared.Models
+{
+    public class ShelterDatabaseContext
+    {
+        private readonly IMongoDatabase _mongoDatabase;
+
+
+        public ShelterDatabaseContext()
+        {
+            // Opretter en MongoDB-client med forbindelse til MongoDB Atlas
+            var client = new MongoClient("mongodb+srv://admindb:6!bWGg_i62ugLEJ@cluster0.zvgfl.mongodb.net/shelterdb?retryWrites=true&w=majority");
+
+            // Henter shelter-databasen fra client
+            _mongoDatabase = client.GetDatabase("shelterdb");
+
+        }
+
+
+
+        // Henter shelters fra _mongoDatabase ("shelterdb")
+        public IMongoCollection<Shelter> ShelterCollection
+        {
+            get
+            {
+                return _mongoDatabase.GetCollection<Shelter>("shelters");
+            }
+        }
+
+
+
+        // Henter bookings fra _mongoDatabase ("shelterdb")
+        public IMongoCollection<Booking> BookingCollection
+        {
+            get
+            {
+                return _mongoDatabase.GetCollection<Booking>("bookings");
+            }
+        }
+
+    }
+}
